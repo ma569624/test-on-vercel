ba47fe7e396a4c052141ec01b6298b742f9ac2be
manish <ms569624@gmail.com>
2024-04-09 21:59:21 +0530

    try to deploy 10

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'new-backend-server-production.up.railway.app']
+        domains: ['new-backend-server-production.up.railway.app']
     }
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -5,21 +5,19 @@ import Home from './Home';
 
 export default async function Main() {
   const API = process.env.API
-  let toplinks, advert, todaynews, badikhabar;
-    toplinks = await fetch(`${API}/api/toplinks`).then((res) => res.json())
-    advert = await fetch(`${API}/api/advert?Status=active`).then((res) => res.json())
-    todaynews = await fetch(`${API}/api/blogs?Status=active&Category=primenews`).then((res) => res.json())
-    badikhabar = await fetch(`${API}/api/blogs?Status=active&Category=mainnews`).then((res) => res.json())
+    // let toplinks, advert, todaynews, badikhabar;
+    // toplinks = await fetch(`${API}/api/toplinks`).then((res) => res.json())
+    // advert = await fetch(`${API}/api/advert?Status=active`).then((res) => res.json())
+    // todaynews = await fetch(`${API}/api/blogs?Status=active&Category=primenews`).then((res) => res.json())
+    // badikhabar = await fetch(`${API}/api/blogs?Status=active&Category=mainnews`).then((res) => res.json())
  
-
-
 
   return (
 
     <> 
      <h1>{API}</h1>
      
-     <Home API={API} toplinks={toplinks} advert={advert} badikhabar={badikhabar.data} todaynews={todaynews.data} />
+     {/* <Home API={API} toplinks={toplinks} advert={advert} badikhabar={badikhabar.data} todaynews={todaynews.data} /> */}
     </>
   );
 }
