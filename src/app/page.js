68e637ad3c008eade7ccf0c32dc84979c897b268
manish <ms569624@gmail.com>
2024-04-10 18:45:51 +0530

    try to deploy 61

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,34 @@
-import Image from "next/image";
-import Home from "./_component/home";
-
 
 
 export default async function Main() {
-  const API = process.env.API
-  // let toplinks, advert, todaynews, badikhabar;
-  let toplinks = await fetch(`${API}/api/toplinks`).then((res) => res.json())
-  // let advert = await fetch(`${API}/api/advert?Status=active`).then((res) => res.json())
-  let todaynews = await fetch(`${API}/api/blogs?Status=active&Category=primenews`).then((res) => res.json())
-  let badikhabar = await fetch(`${API}/api/blogs?Status=active&Category=mainnews`).then((res) => res.json())
+  const API = process.env.API;
+  let toplinks = [];
+  let todaynews = [];
+  let badikhabar = [];
+
+  try {
+    toplinks = await fetch(`${API}/api/toplinks`).then((res) => res.json());
+    todaynews = await fetch(`${API}/api/blogs?Status=active&Category=primenews`).then((res) => res.json());
+    badikhabar = await fetch(`${API}/api/blogs?Status=active&Category=mainnews`).then((res) => res.json());
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 
   return (
-
     <>
-      <h1> this is api {API} {
-        toplinks.map((item) => <>{item._id} <br></br></>)
-      }</h1>
-      <h1> this is api for advert {API} {
-        todaynews.data.map((item) => <>{item._id} <br></br></>)
-      }</h1>
-      {/* <h1> this is api for advert {API} {
-        badikhabar.data.map((item) => <>{item._id}</>)
-      }</h1> */}
-      {/* <Home API={API} toplinks={toplinks} advert={advert} badikhabar={badikhabar.data} todaynews={todaynews.data} /> */}
+      <h1>API for toplinks: {API}</h1>
+      <ul>
+        {toplinks.map((item) => (
+          <li key={item._id}>{item._id}</li>
+        ))}
+      </ul>
+
+      <h1>API for today's news: {API}</h1>
+      <ul>
+        {todaynews.data.map((item) => (
+          <li key={item._id}>{item._id}</li>
+        ))}
+      </ul>
     </>
   );
 }
