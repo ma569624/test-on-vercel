b1f2f594de283a6665d671bfcffde5b85a6a8950
manish <ms569624@gmail.com>
2024-04-10 18:18:29 +0530

    try to deploy 5

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,15 @@ import Image from "next/image";
 
 export default async function Main() {
   const API = process.env.API
- 
+  let toplinks = await fetch(`${API}/api/toplinks`).then((res) => res.json())
+
   return (
 
     <>
-      <h1>{API}</h1>
+      <h1> this is api {API} {
+        toplinks.map((item) => <>{item._id}</>)
+      }</h1>
+      {/* <Home API={API} toplinks={toplinks} advert={advert} badikhabar={badikhabar.data} todaynews={todaynews.data} /> */}
     </>
   );
 }
