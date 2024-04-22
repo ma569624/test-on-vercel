de4990173c1f4908805c6e2611ed41538d4d3a58
manish <ms569624@gmail.com>
2024-04-23 00:18:51 +0530

    try to show color

--- src/app/inner/[...id]/_component/detail.js
+++ src/app/inner/[...id]/_component/detail.js
@@ -253,7 +253,6 @@ const Detail = (props) => {
                     src={`${API}${sidenamerajiya.Image1}`}
                   />
                 )}
-                {/* sjbhf */}
                 <h2 className="text-center mb-0">{data.Category}</h2>
               </div>
               <div className="post-details ">
