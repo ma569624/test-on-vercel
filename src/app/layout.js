7e76b5725bbec23d6edde7c25836ed00dc346933
manish <ms569624@gmail.com>
2024-04-23 12:04:25 +0530

    try to show color

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,7 @@ export const metadata = {
 const API = process.env.API;
 
 async function fetchtoplinks() {
-  const res = await fetch(`${process.env.API}/api/toplinks`, {
-    cache: "no-store",
-  });
+  const res = await fetch(`${process.env.API}/api/toplinks`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -23,9 +21,7 @@ async function fetchtoplinks() {
 }
 async function fetchAdvert() {
   try {
-    const advert = await fetch(`${process.env.API}/api/advert?Status=active`, {
-      cache: "no-store",
-    });
+    const advert = await fetch(`${process.env.API}/api/advert?Status=active`);
     return advert.json();
   } catch (error) {
     console.error("Error fetching advert:", error);
@@ -33,38 +29,32 @@ async function fetchAdvert() {
   }
 }
 
-// async function fetchTagline() {
-//   try {
-//     const tagline = await fetch(
-//       `${process.env.API}/api/tagline?_id=6602a87711e47f88c9059347`,
-//       { cache: "no-store" }
-//     );
-//     return tagline.json();
-//   } catch (error) {
-//     console.error("Error fetching tagline:", error);
-//     throw error;
-//   }
-// }
+async function fetchTagline() {
+  try {
+    const tagline = await fetch(
+      `${process.env.API}/api/tagline?_id=6602a87711e47f88c9059347`);
+    return tagline.json();
+  } catch (error) {
+    console.error("Error fetching tagline:", error);
+    throw error;
+  }
+}
 
-// async function fetchTopKhabare() {
-//   try {
-//     const topKhabare = await fetch(
-//       `${process.env.API}/api/blogs?Category=TopKhabare&Status=active`,
-//       { cache: "no-store" }
-//     );
-//     return topKhabare.json();
-//   } catch (error) {
-//     console.error("Error fetching top khabare:", error);
-//     throw error;
-//   }
-// }
+async function fetchTopKhabare() {
+  try {
+    const topKhabare = await fetch(
+      `${process.env.API}/api/blogs?Category=TopKhabare&Status=active`);
+    return topKhabare.json();
+  } catch (error) {
+    console.error("Error fetching top khabare:", error);
+    throw error;
+  }
+}
 
 async function fetchIdharbhi() {
   try {
     const idharbhi = await fetch(
-      `${process.env.API}/api/blogs?Category=idharbhi&Status=active`,
-      { cache: "no-store" }
-    );
+      `${process.env.API}/api/blogs?Category=idharbhi&Status=active`);
     return idharbhi.json();
   } catch (error) {
     console.error("Error fetching idharbhi:", error);
@@ -72,13 +62,32 @@ async function fetchIdharbhi() {
   }
 }
 
+async function fetchAllBlogs() {
+  try {
+    const allblogs = await fetch(`${process.env.API}/api/allblogs?name=block`);
+    return allblogs.json();
+  } catch (error) {
+    console.error("Error fetching all blogs:", error);
+    throw error;
+  }
+}
 
+async function fetchRajiyablogs() {
+  try {
+    const Rajiyablogs = await fetch(
+      `${process.env.API}/api/allblogs?name=rajiya`
+    );
+    return Rajiyablogs.json();
+  } catch (error) {
+    console.error("Error fetching Rajiyablogs:", error);
+    throw error;
+  }
+}
 
 async function fetchtodaynews() {
   try {
     const todaynews = await fetch(
-      `${API}/api/blogs?Status=active&Category=primenews`,
-      { cache: "no-store" }
+      `${API}/api/blogs?Status=active&Category=primenews`
     );
     return todaynews.json();
   } catch (error) {
@@ -90,8 +99,7 @@ async function fetchtodaynews() {
 async function fetchbadikhabar() {
   try {
     const badikhabar = await fetch(
-      `${API}/api/blogs?page=1&limit=12&Status=active&Category=mainnews`,
-      { cache: "no-store" }
+      `${API}/api/blogs?page=1&limit=12&Status=active&Category=mainnews`
     );
     return badikhabar.json();
   } catch (error) {
@@ -111,22 +119,23 @@ export default async function RootLayout({ children }) {
 
   const toplinks = await fetchtoplinks();
   const advert = await fetchAdvert();
-  // const tagline = await fetchTagline();
-  // const topKhabare = await fetchTopKhabare();
+  const tagline = await fetchTagline();
+  const topKhabare = await fetchTopKhabare();
   const idharbhi = await fetchIdharbhi();
-  // const allblogs = await fetchAllBlogs();
-  // const Rajiyablogs = await fetchRajiyablogs();
+  const allblogs = await fetchAllBlogs();
+  const Rajiyablogs = await fetchRajiyablogs();
   
   return (
     <html lang="en">
       <body>
-        <AppProvider>
+        <AppProvider >
           <Header
             API={API}
             advert={advert}
             tagline={tagline}
-            // allblogs={allblogs.data}
-            // Rajiyablogs={Rajiyablogs.data}
+            allblogs={allblogs.data}
+            Rajiyablogs={Rajiyablogs.data}
+
             toplinks={toplinks}
             topKhabare={topKhabare.data}
             todaynews={todaynews.data}
@@ -134,14 +143,14 @@ export default async function RootLayout({ children }) {
           />
 
           {children}
-          {/* <Footer
+          <Footer
             API={API}
             advert={advert}
             idharbhi={idharbhi.data}
             toplinks={toplinks}
-            // allblogs={allblogs.data}
-            // Rajiyablogs={Rajiyablogs.data}
-          /> */}
+            allblogs={allblogs.data}
+            Rajiyablogs={Rajiyablogs.data}
+          />
         </AppProvider>
       </body>
     </html>
